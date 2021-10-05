b288c408577963cd0a8fc10d10bb86663c6b0055
Parsa Joneidi <parsajoneidy@gmail.com>
2021-10-05 20:21:04 +0330

    routes implemented

--- start/routes.js
+++ start/routes.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { route } = require('@adonisjs/framework/src/Route/Manager')
+
 /*
 |--------------------------------------------------------------------------
 | Routes
@@ -17,3 +19,73 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+
+Route.post('/createbook','BookController.create').as('book.create')
+
+Route.post('/rate/:book_id','BookController.rate').as('book.rate')
+
+Route.post('/like/:book_id/:like_type','BookController.like').as('book.like')
+
+Route.post('/comment/:book_id?/:quote_id?','BookController.comment').as('book.comment')
+
+Route.get('/book/single/:book_id','BookController.single_book').as('book.single_book')
+
+Route.group(() => {
+        Route.get('all','BookController.all_books').as('book.all')
+
+        Route.post('add/reading/:book_id','BookController.add_to_reading_list').as('book.add_to_reading_list')
+
+        Route.post('add/read/:book_id','BookController.add_to_read_list').as('book.add_to_read_list')
+
+        Route.post('add/mark/:book_id','BookController.mark').as('book.mark')
+
+        Route.delete('rmv/reading/:book_id','BookController.remove_from_reading_list').as('remove_from_reading_list')
+
+        Route.delete('rmv/read/:book_id','BookController.remove_from_read_list').as('book.remove_from_read_list')
+
+        Route.post('move/:book_id','BookController.move_to_read').as('book.move')
+
+        Route.get('my','BookController.my_books').as('book.my')
+
+        Route.get('find/:book_name',"BookController.search").as('book.find')
+
+        Route.get('cover/:book_id','BookController.cover_image').as('book.cover')
+
+        Route.get('journeys/:book_id','JourneyController.books_journeys').as('book.journeys')
+
+        Route.get('reports','ReportController.books_reports').as('book.reports')
+}).prefix('book')
+
+Route.group(() => {
+        Route.post('create/:story_id','JourneyController.create').as('journey.create')
+
+        Route.get('reports','ReportController.journeys_reports').as('journey.reports')
+})
+
+Route.group(() => {
+        Route.post('create','QuoteController.create').as('quote.create')
+
+        Route.delete('delete','QuoteController.delete').as('quote.delete')
+}).prefix('quote')
+
+Route.group(() => {
+        Route.post('report/create/:type/:id','ReportController.create').as('report.create')
+
+        Route.get('all','ReportController.all_reports').as('report.all')
+
+        Route.get('book','ReportController.single_book').as('report.book')
+
+        Route.get('journey','ReportController.single_journey').as('report.journey')
+
+        Route.get('user','ReportController.single_user').as('report.user')
+
+        Route.delete('delete','ReportController.delete_report').as('report.dismiss')
+
+        Route.delete('delete/book','ReportController.delete_book').as('report.delete_book')
+
+        Route.delete('delete/journey','ReportController.delete_journey').as('report.delete_journey')
+
+        Route.delete('delete/user','ReportController.delete_user').as('report.delete_user')
+}).prefix('report')
+
+
