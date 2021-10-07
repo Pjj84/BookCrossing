884ab7e598250c71352d195878844efc71998517
Parsa Joneidi <parsajoneidy@gmail.com>
2021-10-07 20:01:26 +0330

    book controller bug fixed

--- app/Controllers/Http/BookController.js
+++ app/Controllers/Http/BookController.js
@@ -6,6 +6,7 @@ const Comment = use("App/Models/Comment")
 const Notif = use("App/Models/Notification")
 const Database = use("Database")
 const Rating = use("App/Models/Rating")
+const Like = use('App/Models/Like')
 
 class BookController {
     async create({request, response, params, auth}){
@@ -14,6 +15,8 @@ class BookController {
                 return response.status(400).send()
             }
             const user = await auth.getUser()
+            const exact_same_book = await Book.query().where('isbn',request.input('isbn')).first()
+            if( exact_same_book ){ return response.status(403).send() } // Already exists
             const book = new Book
             book.name = request.input('name')
             book.owner_id = user.id
@@ -28,7 +31,6 @@ class BookController {
                 book.cover_image = date
             }
             book.status = "pending"
-            book.rates = 0
             book.isbn = request.input('isbn')
             const book_id = await book.save()
             const story = new Story
@@ -63,22 +65,22 @@ class BookController {
             try{
                 rating = parseInt(request.input('rating'))
             }catch(e){
-                return response.status(432).send() // 432 status code means the input is not integer
+                return response.status(410).send() // 410 status code means the input is not integer
             }
-            const book = await Book.findOrFail(params.book_id)
+            const book = await Book.find(params.book_id)
             const user = await auth.getUser()
-            if( !book ){ return response.status(400) }
+            if( !book ){ return response.status(404).send() }
             let rate = await Rating.query().where('user_id',user.id).first()
             if( !rate){ 
                 rate = new Rating
                 rate.book_id = book.id
                 rate.user_id = user.id
-                rate.rating = rating
+                rate.rate = rating
             }else{
-                rate.rating = rating
+                rate.rate = rating
             }
-            if( rate.rating < 0 || rate.rating > 10){ throw new Error }
-            rate.save()
+            if( rate.rate < 0 || rate.rate > 10){ throw new Error }
+            await rate.save()
             return response.status(200).send()
         //}catch(e){
            // return response.status(500)
@@ -95,42 +97,61 @@ class BookController {
         //}
     }
     async like({request, response, params, auth}){
-        try{
+        //try{
             const like = new Like       
             const user = await auth.getUser()
             like.user_id = user.id
             like.book_id = params.book_id
-            like.type = params.like_type
+            if( params.like_type == 1){ like.type = 'like' }
+            else if( params.like_type == 0){ like.type = 'dislike' }
             like.save()
-            return response.status(200)
-        }catch(e){
-            return response.status(500)
-        }
+            return response.status(200).send()
+        //}catch(e){
+          //  return response.status(500).send()
+        //}
     }
-    async comment([request, response, params, auht]){
-        try{
+    async show_likes({request, response, params, auth}){
+        //try{
+        const likes = await Database.from('likes').where('book_id',params.book_id).where('type','like').count()
+        const dislikes = await Database.from('likes').where('book_id',params.book_id).where('type','dislike').count()
+        return response.status(200).json({likes: likes,dislikes: dislikes})
+        //}catch(e){
+            //return response.status(500).send()
+        //}
+    }
+    async comment({request, response, params, auth}){
+        //try{
             const comment = new Comment
             const user = await auth.getUser()
+            const query = request.get()
             if( params.book_id ){
                 comment.book_id = params.book_id
             }else if( params.quote_id ){
                 comment.quote_id = params.quote_id
             }
             comment.user_id = user.id
-            if( !request.input('comment_text') ){
-                return reespones.status(400)
+            if( !request.input('text') ){
+                return reespones.status(400).send()
             }
             comment.text = request.input('text')
-            if( params.replying_to ){
-                comment.replying_to = params.replying_to
+            if( query.replying_to ){
+                comment.replying_to = query.replying_to
             }
             comment.save()
-            return response.status(200)
-        }catch(e){
-            return response.status(500)
-        }
+            return response.status(200).send()
+        //}catch(e){
+          //  return response.status(500)
+        //}
     }
-
+    async show_comments({request, response, params, auth}){
+        //try{
+            const book = await Book.find(params.book_id)
+            const comments = await Comment.query().where('book_id',book.id).fetch()
+            return response.status(200).json({comments: comments})
+        //}catch(e){
+            //return response.status(500).send()
+        //}
+    }
     async single_book({request, response, params, auth}){
         try{
             const book = await Book.findOrFail(params.book_id)
@@ -151,19 +172,24 @@ class BookController {
         }
     }
     async add_to_reading_list({request, response, params, auth}){
-        try{
+        //try{
             const user = await auth.getUser()
-            const book = await Book.findOrFail(params.book_id)
-            if( !book ){ return response.status(400) }
-            if( !request.input('isbn') || request.input('isbn') != book.isbn ){ return response.status(401) }
-            let reading_books = user.reading_books
-            reading_books += "," + book.id.toString()
-            user.reading_books = reading_books 
+            const book = await Book.find(params.book_id)
+            if( !book ){ return response.status(404).send() }
+            if( !request.input('isbn') || parseInt(request.input('isbn')) != book.isbn ){ return response.status(401).send() }
+            let reading_books = user.reading_books ? user.reading_books.split(',') : []
+            if( !reading_books.includes(book.id.toString()) ){
+                reading_books.push(book.id.toString())
+            }else if( reading_books.includes(book.id.toString()) ){
+                reading_books.splice(reading_books.indexOf(book.id.toString()),1)
+                if( reading_books == "" ){ reading_books = null}
+            }//else{ return response.status(500) } 
+            user.reading_books = reading_books ? reading_books.join(',') : null 
             user.save()
-            return response.status(200)
-        }catch(e){
-            return response.status(500)
-        }
+            return response.status(200).json({reading_books: reading_books,type: typeof book.id})
+        //}catch(e){
+          //  return response.status(500).send()
+        //}
     }
     async add_to_read_list({request, response, params, auth}){
         try{
